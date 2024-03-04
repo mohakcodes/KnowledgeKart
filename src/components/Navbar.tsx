694ba6f38f41ff34f60a7e3c792a0e2fdf92a069
Mohak Chauhan <mohak46812@gmail.com>
2024-03-04 15:08:51 +0530

    Add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+export default function Navbar(){
+    return(
+        <div className="navbar bg-gray-800 relative z-50">
+        <div className="flex-1">
+            <a className="btn btn-ghost text-xl">E-Commerce</a>
+        </div>
+        <div className="flex-none">
+            <ul className="menu menu-horizontal px-4">
+            <li><a>Login</a></li>
+            <li>
+                <details>
+                <summary>
+                    More
+                </summary>
+                <ul className="p-2 rounded-t-none bg-slate-700">
+                    <li><a>Edit</a></li>
+                    <li><a>Logout</a></li>
+                </ul>
+                </details>
+            </li>
+            </ul>
+        </div>
+        </div>
+    )
+}
