d2bd19d6b002b4a9349193795600e2d7137da98a
Mohak Chauhan <mohak46812@gmail.com>
2024-03-09 02:42:42 +0530

    fix logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,15 +1,9 @@
-"use client"
-
-import { useUserStore } from "@/app/utils/store";
 import { NextResponse } from "next/server";
-
-const setUser = useUserStore((state)=>state.setUser);
 
 export async function GET(){
     try {
         const response = NextResponse.json({message:"Logout Successful",success:true});
         response.cookies.set("token","",{httpOnly:true,expires:new Date(0)});
-        setUser(null);
         return response;   
     } 
     catch (error) {
