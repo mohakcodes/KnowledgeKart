cafd212f811116d786f4371a30307cc61692fbd4
Mohak Chauhan <mohak46812@gmail.com>
2024-03-10 20:20:42 +0530

    fix: image size in productcard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,9 +5,12 @@ type Props = {
 }
 
 export default function ProductCard({product}:Props){
+
     return(
         <div className="card card-compact w-90 bg-gray-900 shadow-xl">
-        <figure><img src={product.image} alt="img" /></figure>
+        <figure className="h-44 overflow-hidden">
+            <img src={product.image} alt="img" className="w-full h-full object-cover"/>
+        </figure>
         <div className="card-body">
             <div className="">
                 <h3 className="card-title text-[16px] whitespace-nowrap overflow-ellipsis overflow-hidden">{product.name}</h3>
@@ -20,8 +23,16 @@ export default function ProductCard({product}:Props){
             <p>{product.description.length <= 70 ? product.description : product.description.substring(0,70)}...</p>
             </div>
             <div className="card-actions justify-between font-semibold">
-                <button className="px-3 py-2 bg-primary text-black rounded-md">Read More</button>
-                <button className="px-3 py-2 bg-primary text-black rounded-md">Add to Cart</button>
+                <button 
+                    className="px-3 py-2 bg-primary text-black rounded-md"
+                >
+                        Read More
+                </button>
+                <button 
+                    className="px-3 py-2 bg-primary text-black rounded-md"
+                >
+                    Add to Cart
+                </button>
             </div>
         </div>
         </div>
