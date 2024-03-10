904d971a7b99113f7f44870012cd0dc5224a8ebb
Mohak Chauhan <mohak46812@gmail.com>
2024-03-10 20:09:51 +0530

    update home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -160,19 +160,12 @@ export default function Home(){
                     </div>
                 </div>
                 <div>
-                    {
-                        (Number(range.minRange) >= 0 || 
-                        Number(range.maxRange) >= 0 || 
-                        checkedBrands.length > 0) &&
-                        (
-                            <button 
-                                className="py-2 px-5 bg-orange-600 text-black m-2 rounded-lg"
-                                onClick={(e)=>{handleAllFilters(e)}}
-                            >
-                                Apply Filter
-                            </button>
-                        )
-                    }
+                    <button 
+                        className="py-2 px-5 bg-orange-600 text-black m-2 rounded-lg mt-6"
+                        onClick={(e)=>{handleAllFilters(e)}}
+                    >
+                        Apply Filter
+                    </button>
                 </div>
             </div>
 
